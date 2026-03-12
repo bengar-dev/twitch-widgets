4c635422fa9e3d158f1ba6e05b506b170a97e8ae
Benoit Garcia <benoitgar.pro@gmail.com>
2026-03-12 22:56:30 +0100

    init: tailwind

---
+++ frontend/app/layouts/auth.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="min-h-screen w-full flex flex-col items-center justify-center bg-dark-secondary text-primary font-mono"
+  >
+    <div class="px-4 py-6 bg-dark-secondary border border-dark-third rounded">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ frontend/app/pages/signin/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Sign in page</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "auth",
+});
+</script>
